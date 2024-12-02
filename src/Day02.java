163bccf692ea7aa6cd7592146685e7a0dac7218f
_ <ryancpersonal10@gmail.com>
2024-12-02 01:30:13 -0500

    Day02 completed

--- src/Day02.java
+++ src/Day02.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Collections;
 
@@ -28,16 +29,111 @@ public class Day02 {
         }
     }
     public static void partOne(ArrayList<String> data) {
-        /*
-            @TO BE IMPLEMENTED
-        */
+        int safeNumbers = 0;
+        for (String sample : data) {
+            String[] line = sample.split(" ");
+            boolean safe = true;
+            boolean isIncreasing = Integer.parseInt(line[0]) - Integer.parseInt(line[1]) > 0;
+
+            for (int i = 0; i < line.length - 1; i++){
+                if (Integer.parseInt(line[i]) - Integer.parseInt(line[i+1]) < 0){
+                    if (isIncreasing){
+                        safe = false;
+                    }
+                }
+                if (Integer.parseInt(line[i]) - Integer.parseInt(line[i+1]) > 0){
+                    if (!isIncreasing){
+                        safe = false;
+                    }
+                }
+                if (Integer.parseInt(line[i]) - Integer.parseInt(line[i+1]) == 0){
+                    safe = false;
+                }
+                if (Math.abs(Integer.parseInt(line[i]) - Integer.parseInt(line[i+1])) > 3 ){
+                    safe = false;
+                }
+            }
+            if (safe){
+                safeNumbers++;
+            }
+        }
+        System.out.println(safeNumbers);
     }
 
 
     public static void partTwo(ArrayList<String> data) {
-        /*
-            @TO BE IMPLEMENTED
-        */
-    }
+        int safeNumbers = 0;
+        for (String sample : data) {
+            String[] line = sample.split(" ");
+            ArrayList<Integer> numbers = new ArrayList<>();
+            for (String num : line) {
+                numbers.add(Integer.parseInt(num));
+            }
 
+            boolean safe = true;
+            boolean isIncreasing = Integer.parseInt(line[0]) - Integer.parseInt(line[1]) > 0;
+
+            for (int i = 0; i < line.length - 1; i++){
+                if (Integer.parseInt(line[i]) - Integer.parseInt(line[i+1]) < 0){
+                    if (isIncreasing){
+                        safe = false;
+                    }
+                }
+                if (Integer.parseInt(line[i]) - Integer.parseInt(line[i+1]) > 0){
+                    if (!isIncreasing){
+                        safe = false;
+                    }
+                }
+                if (Integer.parseInt(line[i]) - Integer.parseInt(line[i+1]) == 0){
+                    safe = false;
+                }
+                if (Math.abs(Integer.parseInt(line[i]) - Integer.parseInt(line[i+1])) > 3 ){
+                    safe = false;
+                }
+            }
+
+
+            if (!safe) {
+                for (int i = 0; i < numbers.size(); i++) {
+                    ArrayList<Integer> modified = new ArrayList<>(numbers);
+                    modified.remove(i);
+
+                    safe = true;
+                    isIncreasing = modified.get(1) > modified.get(0);
+
+                    for (int j = 0; j < modified.size() - 1; j++) {
+                        if (modified.get(j + 1) - modified.get(j) > 0) {
+                            if (!isIncreasing) {
+                                safe = false;
+                                break;
+                            }
+                        } else if (modified.get(j + 1) - modified.get(j) < 0) {
+                            if (isIncreasing) {
+                                safe = false;
+                                break;
+                            }
+                        }
+                        if (modified.get(j + 1) - modified.get(j) == 0) {
+                            safe = false;
+                            break;
+                        }
+                        if (Math.abs(modified.get(j + 1) - modified.get(j)) > 3){
+                            safe = false;
+                            break;
+                        }
+                    }
+
+                    if (safe) {
+                        break;
+                    }
+                }
+            }
+
+            if (safe) {
+                safeNumbers++;
+            }
+        }
+
+        System.out.println(safeNumbers);
+    }
 }
