01b14448ca885a3b1ad5651f719989a9a68e0f7f
_ <ryancpersonal10@gmail.com>
2024-12-03 01:08:32 -0500

    Day04.java is prepped for tomorrow.

---
+++ src/Day04.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class Day04 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("inputs/Day04Input.txt");
+        partOne(fileData);
+        partTwo(fileData);
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+    public static void partOne(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+
+    public static void partTwo(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+}
