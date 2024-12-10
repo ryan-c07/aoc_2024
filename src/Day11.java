3acceb2ec7e45149a9352c01a2cb8ed8006ed7f1
_ <ryancpersonal10@gmail.com>
2024-12-10 06:53:15 -0500

    Day11.java is prepped for tomorrow.

---
+++ src/Day11.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day11 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = StarterMethods.getFileData("inputs/Day11Input.txt");
+        partOne(fileData);
+        partTwo(fileData);
+    }
+
+    public static void partOne(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+
+    public static void partTwo(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+}
