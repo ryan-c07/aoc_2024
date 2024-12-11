4ea6902d0aaf4563c1e4f1ee4a7b898aeef74d3c
_ <ryancpersonal10@gmail.com>
2024-12-11 18:48:54 -0500

    Day12.java is prepped for tomorrow.

---
+++ src/Day12.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Day12 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = StarterMethods.getFileData("inputs/Day12Input.txt");
+        partOne(fileData);
+        partTwo(fileData);
+    }
+
+
+    public static void partOne(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+
+    public static void partTwo(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+}
