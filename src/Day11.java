ff436dd4f20e368d2e880f75bdb89c7df04339eb
_ <ryancpersonal10@gmail.com>
2024-12-11 05:03:46 -0500

    Day11 Part One completed

--- src/Day11.java
+++ src/Day11.java
@@ -1,26 +1,47 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
 
 public class Day11 {
     public static void main(String[] args) {
         ArrayList<String> fileData = StarterMethods.getFileData("inputs/Day11Input.txt");
-        partOne(fileData);
+        System.out.println(fileData);
+//        partOne(fileData);
         partTwo(fileData);
     }
 
     public static void partOne(ArrayList<String> data) {
-        /*
-            @TO BE IMPLEMENTED
-        */
+        int count = 0;
+        while (count < 25){
+            for (int i = 0; i < data.size(); i++){
+                String sample = data.get(i);
+                sample = String.valueOf(Long.parseLong(sample));
+                if (sample.length() % 2 == 0){
+                    String left = sample.substring(0, sample.length()/2);
+                    String right = sample.substring(sample.length()/2); // change leading zeros
+                    data.set(i, left);
+                    data.add(i+1, right);
+                    i++;
+                }
+                else {
+                    for (int j = 0; j < sample.length(); j++){
+                        if (sample.equals("0")){
+                            data.set(i, "1");
+                        }
+                        else {
+                            data.set(i, String.valueOf(Long.parseLong(sample)*2024));
+                        }
+                    }
+                }
+            }
+            count++;
+        }
+        System.out.println(data.size());
     }
 
 
     public static void partTwo(ArrayList<String> data) {
         /*
-            @TO BE IMPLEMENTED
-        */
+        * idk i probably have to rewrite part1
+        * */
     }
-
 }
