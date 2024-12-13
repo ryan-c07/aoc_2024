d64e79ebda58fb2d0945987e53f46cc35d583922
_ <ryancpersonal10@gmail.com>
2024-12-12 20:58:06 -0500

    Day12 Part One completed

--- src/Day12.java
+++ src/Day12.java
@@ -1,24 +1,87 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Day12 {
+    private static String[][] fileData = StarterMethods.getFileDataBorder("inputs/Day12Input.txt", 5);
+    private static String[][] tempData = new String[fileData.length][fileData[0].length];
+
     public static void main(String[] args) {
-        ArrayList<String> fileData = StarterMethods.getFileData("inputs/Day12Input.txt");
-        partOne(fileData);
-        partTwo(fileData);
+        for (String[] arr : tempData) {
+            Arrays.fill(arr, ".");
+        }
+        for (String[] row : fileData){
+            System.out.println(Arrays.toString(row));
+        }
+        String[][] copy = new String[fileData.length][fileData[0].length];
+        for (int i = 0; i < fileData.length; i++) {
+            for (int j = 0; j < fileData[0].length; j++) {
+                copy[i][j] = fileData[i][j];
+            }
+        }
+        partOne();
+        fileData = copy;
+        partTwo();
     }
 
+    public static void partOne() {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int total = 0;
+        while (!alphabet.isEmpty()) {
+            String currentLetter = alphabet.substring(0,1);
+            for (int row = 0; row < fileData.length; row++) {
+                for (int col = 0; col < fileData[0].length; col++) {
+                    if (fileData[row][col].equals(currentLetter)) {
+                        findShapes(row, col, currentLetter);
+                        int perimeter = 0;
+                        int area = 0;
+                        for (int r = 0; r < fileData.length; r++) {
+                            for (int c = 0; c < fileData[0].length; c++) {
+                                if (tempData[r][c].equals(currentLetter)) {
+                                    if (!tempData[r][c-1].equals(currentLetter)) {
+                                        perimeter++;
+                                    }
+                                    if (!tempData[r][c+1].equals(currentLetter)) {
+                                        perimeter++;
+                                    }
+                                    if (!tempData[r-1][c].equals(currentLetter)) {
+                                        perimeter++;
+                                    }
+                                    if (!tempData[r+1][c].equals(currentLetter)) {
+                                        perimeter++;
+                                    }
+                                    area++;
+                                }
+                            }
+                        }
+                        total += area*perimeter;
+                        for (String[] arr : tempData) {
+                            Arrays.fill(arr, ".");
+                        }
+                    }
+                }
+            }
+            alphabet = alphabet.substring(1);
+        }
+        System.out.println(total);
+    }
 
-    public static void partOne(ArrayList<String> data) {
+    public static void partTwo() {
         /*
-            @TO BE IMPLEMENTED
-        */
+        * TO BE IMPLEMENTED
+        * */
     }
 
 
-    public static void partTwo(ArrayList<String> data) {
-        /*
-            @TO BE IMPLEMENTED
-        */
-    }
+    public static void findShapes(int row, int col, String letter) {
+        if (!fileData[row][col].equals(letter) ||
+                tempData[row][col].equals(letter)) {
+            return;
+        }
 
+        tempData[row][col] = letter;
+        fileData[row][col] = ".";
+        findShapes(row - 1, col, letter);
+        findShapes(row, col - 1, letter);
+        findShapes(row + 1, col, letter);
+        findShapes(row, col + 1, letter);
+    }
 }
--- src/StarterMethods.java
+++ src/StarterMethods.java
@@ -1,9 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class StarterMethods {
+
 
     public static ArrayList<String> getFileData(String fileName) { //1D arrays
         ArrayList<String> fileData = new ArrayList<String>();
@@ -21,6 +24,7 @@ public class StarterMethods {
             return fileData;
         }
     }
+
 
     public static String[][] getFileData(String fileName, int size) { //2D arrays
         String[][] fileData = new String[size][size];
@@ -42,4 +46,31 @@ public class StarterMethods {
             return new String[size][size];
         }
     }
+
+
+    public static String[][] getFileDataBorder(String fileName, int size) { //2D arrays with borders
+        String[][] fileData = new String[size+2][size+2];
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            int count = 1;
+            while (s.hasNext()) {
+                String line = s.nextLine();
+                if (!line.isEmpty()) {
+                    for (int i = 0; i < line.length(); i++) {
+                        fileData[count][i+1] = line.substring(i, i+1);
+                    }
+                    fileData[count][0] = ".";
+                    fileData[count][size+1] = ".";
+                    count++;
+                }
+            }
+            Arrays.fill(fileData[0], ".");
+            Arrays.fill(fileData[size+1], ".");
+            return fileData;
+        } catch (FileNotFoundException e) {
+            return new String[size][size];
+        }
+    }
 }
+
