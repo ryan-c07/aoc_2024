9b5f9292c8a74d15796678ff844324d0c3140fc5
_ <ryancpersonal10@gmail.com>
2024-12-16 17:46:39 -0500

    Created files and input.txt for all the following days that I have yet to complete

---
+++ src/Day13.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Day13 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = StarterMethods.getFileData("inputs/Day13Input.txt");
+        partOne(fileData);
+        partTwo(fileData);
+    }
+
+
+    public static void partOne(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+
+    public static void partTwo(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+}
---
+++ src/Day16.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Day16 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = StarterMethods.getFileData("inputs/Day16Input.txt");
+        partOne(fileData);
+        partTwo(fileData);
+    }
+
+
+    public static void partOne(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+
+    public static void partTwo(ArrayList<String> data) {
+        /*
+            @TO BE IMPLEMENTED
+        */
+    }
+
+}
